6a460aa2c4d58704294606be21a26b419e10d25f
Scott Barron <scott@barron.io>
2022-02-25 13:34:29 -0500

    cleanup

--- elixir-mode/.yas-setup.el
+++ elixir-mode/.yas-setup.el
@@ -1,3 +1,20 @@
 ;;; snippets/elixir-mode/.yas-setup.el -*- lexical-binding: t; -*-
 
 (defun column-types () (list "string" "uuid" "binary" "decimal" "integer" "text" "char" "varchar"))
+
+(defun module-1 (bufname)
+  (file-name-nondirectory
+   (directory-file-name
+    (file-name-directory bufname)
+    )))
+
+(defun module-2 (bufname)
+  (file-name-nondirectory
+   (directory-file-name
+    (file-name-directory
+     (directory-file-name
+      (file-name-directory bufname)
+      )))))
+
+(defun modularize(name)
+  (mapconcat 'capitalize (split-string name "_") ""))
